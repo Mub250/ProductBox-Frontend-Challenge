1f066c2109cc40488e2551abb8d3dfa28852b4d4
haiderali9 <90863262+haiderali9@users.noreply.github.com>
2022-02-08 01:10:27 +0500

    new

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var debug = require('debug')('frontend-code-challenge1');
+var debug = require('debug')('frontend-code-challenge');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
